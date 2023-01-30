853da4c9d474ff3457be3dc3437150ac08d5cf54
lyra-school <S00233718@atu.ie>
2023-01-30 10:27:15 +0000

    changes

---
+++ Q5/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Name: Lyra Karsaj
+ * Date: 30/1/23
+ * Desc: Array fill
+ */
+namespace Q5
+{
+    internal class Program
+    {
+        static int[] x = new int[6];
+        static void AlltheNines()
+        {
+            for(int i = 0; i < x.Length; i++)
+            {
+                x[i] = 9;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Q6/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Name: Lyra Karsaj
+ * Date: 30/1/23
+ * Desc: Array element counter
+ */
+namespace Q6
+{
+    internal class Program
+    {
+        static int CountGreater(int[] x, int value)
+        {
+            int count = 0;
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
